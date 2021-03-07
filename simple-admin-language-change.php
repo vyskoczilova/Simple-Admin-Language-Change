af12d7b7338e0a323c00d4f30717bb1f2df4fe44
Karolína Vyskočilová <karolina@kybernaut.cz>
2021-03-07 22:58:07 +0100

    List available user languages in the top admin bar

--- simple-admin-language-change.php
+++ simple-admin-language-change.php
@@ -19,7 +19,6 @@ if ( ! defined( 'WPINC' ) ) {
 
 define('SIMPLE_ADMIN_LANGUAGE_VERSION', '2.0.0');
 
-
 /**
  * Localize the plugin
  *
@@ -64,3 +63,92 @@ function simple_admin_language_check_version()
 	}
 }
 add_action('plugins_loaded', 'simple_admin_language_check_version');
+
+function simple_admin_language_admin_menu ( WP_Admin_Bar $admin_bar ) {
+	// Check for permissions matching the user_locale
+	if ( ! current_user_can( 'edit_posts' ) || ! current_user_can( 'edit_pages' ) ) {
+		return;
+    }
+
+	$languages = simple_admin_language_parse_wp_dropdown_languages();
+
+    $admin_bar->add_menu( array(
+		'id'    => 'salc-current-language',
+        'parent' => 'top-secondary',
+        'group'  => null,
+        'title' => '<span class="ab-icon"></span>' . $languages['active']['title'],
+        'href'  => '#',
+        'meta' => [
+			'title' => __( 'Current dashboard language', 'kbnt-kbnt-scal' ),
+		]
+	) );
+
+	foreach( $languages['available'] as $la ) {
+
+		$admin_bar->add_menu([
+			'id'    => 'salc-current-' . sanitize_title($la['title']),
+			'parent' =>'salc-current-language',
+			'group'  => null,
+			'title' => $la['title'],
+			'href'  => '#',
+		]);
+
+	}
+}
+add_action( 'admin_bar_menu', 'simple_admin_language_admin_menu', 500 );
+
+/**
+ * Parse HTML of wp_dropdown_languages into array
+ * @return array
+ */
+function simple_admin_language_parse_wp_dropdown_languages() {
+
+	$user_locale = get_user_locale();
+	$available_languages = get_available_languages();
+	ob_start();
+	wp_dropdown_languages(
+		[
+			'name'                        => 'locale',
+			'id'                          => 'locale',
+			'selected'                    => $user_locale,
+			'languages'                   => $available_languages,
+			'show_available_translations' => false,
+			'show_option_site_default'    => true,
+		]
+	);
+	$wp_dropdown_languages = ob_get_contents();
+	ob_end_clean();
+	$languages = [
+		'active' => [],
+		'available' => []
+	];
+
+	$dom = new DOMDocument();
+	$dom->loadHTML(mb_convert_encoding($wp_dropdown_languages, 'HTML-ENTITIES', 'UTF-8'));
+	$options = $dom->getElementsByTagName('option');
+	for ($i = 0; $i < $options->length; $i++ ) {
+		$language = [
+			'value' => $options->item($i)->getAttribute('value'),
+			'title' => $options->item($i)->nodeValue,
+		];
+		if ($options->item($i)->hasAttribute('selected')) {
+			$languages['active'] = $language;
+		} else {
+			$languages['available'][] = $language;
+		}
+	}
+
+	return $languages;
+
+}
+
+
+function simple_admin_language_admin_css() {
+	echo '<style>
+    #wpadminbar #wp-admin-bar-salc-current-language .ab-icon:before {
+		content: "\f326";
+		top: 3px;
+	}
+	</style>';
+}
+add_action('admin_head', 'simple_admin_language_admin_css');
